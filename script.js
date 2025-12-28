10d488d4c60c58f88c8da473be5942362dc3c899
Mr.Orange88 <m.orange@gmx.de>
2025-12-28 12:30:04 +0100

    Added more features, optimized the code, and finished the quiz app.

--- script.js
+++ script.js
@@ -34,42 +34,40 @@ let questions = [
 ];
 
 let rightAnswer = 0;
-
 let currentQuestion = 0;
+let AUDIO_SUCCESS = new Audio("./assets/sounds/right.mp3");
+let AUDIO_FAIL = new Audio("./assets/sounds/wrong.mp3");
 
 function init() {
   document.getElementById("all-questions").innerHTML = questions.length;
 
   showQuestion();
 }
-function showQuestion() {
-  if (currentQuestion >= questions.length) {
-    document.getElementById("endScreen").style = "";
-    document.getElementById("questionBody").style = "display: none";
-    document.getElementById("totalQuestions").innerHTML = questions.length;
-    document.getElementById("correctAnswers").innerHTML = rightAnswer;
-    document.getElementById("header-image").src = "./assets/trophy.png";
-  } else {
-    let question = questions[currentQuestion];
 
-    document.getElementById("question-number").innerHTML = currentQuestion + 1;
-    document.getElementById("questionText").innerHTML = question["question"];
-    document.getElementById("answer_1").innerHTML = question["answer_1"];
-    document.getElementById("answer_2").innerHTML = question["answer_2"];
-    document.getElementById("answer_3").innerHTML = question["answer_3"];
-    document.getElementById("answer_4").innerHTML = question["answer_4"];
+function showQuestion() {
+  if (gameIsOver()) {
+    showEndScreen();
+  } else {
+    updateProgressBar();
+    updateToNextQuestion();
   }
 }
+
+function gameIsOver() {
+  return currentQuestion >= questions.length;
+}
+
 function answer(selectedAnswer) {
   let question = questions[currentQuestion];
   let selectedAnswerNumber = selectedAnswer.slice(-1);
 
   idOfRightAnswer = "answer_" + question["right_answer"];
 
-  if (selectedAnswerNumber == question["right_answer"]) {
+  if (rightAnswerSelected(selectedAnswerNumber, question)) {
     document
       .getElementById(selectedAnswer)
       .parentNode.classList.add("bg-success");
+    AUDIO_SUCCESS.play();
     rightAnswer++;
   } else {
     document
@@ -78,8 +76,13 @@ function answer(selectedAnswer) {
     document
       .getElementById(idOfRightAnswer)
       .parentNode.classList.add("bg-success");
+    AUDIO_FAIL.play();
   }
   document.getElementById("next-button").disabled = false;
+}
+
+function rightAnswerSelected(selectedAnswerNumber, question) {
+  return selectedAnswerNumber == question["right_answer"];
 }
 
 function nextQuestion() {
@@ -88,6 +91,7 @@ function nextQuestion() {
   resetAnswerButton();
   showQuestion();
 }
+
 function resetAnswerButton() {
   document.getElementById("answer_1").parentNode.classList.remove("bg-success");
   document.getElementById("answer_1").parentNode.classList.remove("bg-danger");
@@ -97,4 +101,39 @@ function resetAnswerButton() {
   document.getElementById("answer_3").parentNode.classList.remove("bg-danger");
   document.getElementById("answer_4").parentNode.classList.remove("bg-success");
   document.getElementById("answer_4").parentNode.classList.remove("bg-danger");
+}
+
+function restartGame() {
+  document.getElementById("header-image").src = "./assets/quiz.png";
+  document.getElementById("endScreen").style = "display: none";
+  document.getElementById("questionBody").style = "";
+  rightAnswer = 0;
+  currentQuestion = 0;
+  showQuestion();
+}
+
+function showEndScreen() {
+  document.getElementById("header-image").src = "./assets/trophy.png";
+  document.getElementById("endScreen").style = "";
+  document.getElementById("questionBody").style = "display: none";
+  document.getElementById("correctAnswers").innerHTML = rightAnswer;
+  document.getElementById("totalQuestions").innerHTML = questions.length;
+}
+
+function updateProgressBar() {
+  let percent = (currentQuestion + 1) / questions.length;
+  percent = Math.round(percent * 100);
+  document.getElementById("progress-bar").style.width = `${percent}%`;
+  document.getElementById("progress-bar").innerHTML = `${percent}%`;
+}
+
+function updateToNextQuestion() {
+  let question = questions[currentQuestion];
+
+  document.getElementById("question-number").innerHTML = currentQuestion + 1;
+  document.getElementById("questionText").innerHTML = question["question"];
+  document.getElementById("answer_1").innerHTML = question["answer_1"];
+  document.getElementById("answer_2").innerHTML = question["answer_2"];
+  document.getElementById("answer_3").innerHTML = question["answer_3"];
+  document.getElementById("answer_4").innerHTML = question["answer_4"];
 }
